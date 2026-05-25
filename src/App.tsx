2186f52f90b7b77bd614fec743ce89fc7dae7e43
gargandiz <gargandzolla10@gmail.com>
2026-05-26 00:00:55 +0300

    Lesson_5

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,11 @@ import {Footer} from "./site/Footer";
 import {NewComponent} from "./NewComponents/NewComponent";
 import {Button} from "./Components/Button";
 
+type FilterType = "all" | "dollar" | "ruble"
 
 function App() {
+
+//=====================================================Lesson_2=========================================================
   // const [students, setStudents] = useState([
   //         {id: 1, name: "James", age: 8},
   //         {id: 2, name: "Robert", age: 18},
@@ -22,17 +25,18 @@ function App() {
   //         {id: 11, name: "Christopher", age: 100},
   //     ]
   // )
-//=========================================================================================================
+//=====================================================Lesson_3=========================================================
   // const myFirstSubscriber = (event: MouseEvent<HTMLButtonElement>) => {
   //     console.log("Hello, I'm Vasiliy")
   // }
   // const mySecondSubscriber = (event: MouseEvent<HTMLButtonElement>) => {
   //     console.log("Hello, I'm Ivan")
-//=========================================================================================================
+  // }
+//======================================================================================================================
   // const onClickHandler = (name: string) => {
   //     console.log(name);
   // }
-//=========================================================================================================
+//======================================================================================================================
   // const massage_1 = () => {
   //     //ничего не передавать, но в консоли должно быть +100500
   //     console.log("+100500");
@@ -41,7 +45,7 @@ function App() {
   //     //передать +100500
   //     console.log(message);
   // }
-//=========================================================================================================
+//======================================================================================================================
 //   const Button_1_Foo = (subscriber: string, age: number, address: string) => {
 //     console.log(subscriber, age, address)
 //   }
@@ -51,49 +55,93 @@ function App() {
 //   const Button_3_Foo = () => {
 //     console.log("I'm stupid button")
 //   }
-//=========================================================================================================
-
+//=====================================================Lesson_4=========================================================
     // let a = 1;
-    let [a, setA] = useState(0)
-    const onClickHandlerPlus = () => {
-        setA(++a);
-        console.log(a);
-    }
-    const onClickHandlerZero = () => {
-        setA(a = 0);
-        console.log(a);
-    }
+    // let [a, setA] = useState(0)
+    // const onClickHandlerPlus = () => {
+    //     setA(++a);
+    //     console.log(a);
+    // }
+    // const onClickHandlerZero = () => {
+    //     setA(a = 0);
+    //     console.log(a);
+    // }
+//=====================================================Lesson_5=========================================================
+    const [money, setMoney] = useState([
+        { banknote: 'dollar', nominal: 100, number: ' a1234567890' },
+        { banknote: 'dollar', nominal: 50, number: ' z1234567890' },
+        { banknote: 'ruble', nominal: 100, number: ' w1234567890' },
+        { banknote: 'dollar', nominal: 100, number: ' e1234567890' },
+        { banknote: 'dollar', nominal: 50, number: ' c1234567890' },
+        { banknote: 'ruble', nominal: 100, number: ' r1234567890' },
+        { banknote: 'dollar', nominal: 50, number: ' x1234567890' },
+        { banknote: 'ruble', nominal: 50, number: ' v1234567890' },
+    ])
 
+    const [filter, setFilter] = useState<FilterType>("all")
+
+    let currentMoney = money;
+    if (filter === "dollar") {
+        currentMoney = money.filter((filteredMoney) => filteredMoney.banknote === "dollar");
+    }
+    if (filter === "ruble") {
+        currentMoney = money.filter((filteredMoney) => filteredMoney.banknote === "ruble");
+    }
+    const onClickFilterHandler = (nameButton: FilterType) => {
+        setFilter(nameButton);
+    }
+//=====================================================Lesson_5=========================================================
   return (
       // <>
         <div className="App">
 
-        {/*=========================================================================================================*/}
+{/* =====================================================Lesson_1====================================================*/}
+        {/*<Header title={'New Header'}/>*/}
+        {/*<Body title={'New Body'}/>*/}
+        {/*<Footer title={'new Footer'}/>*/}
+{/* =====================================================Lesson_2====================================================*/}
+        {/*<NewComponent students={students}/>*/}
+{/* =====================================================Lesson_3====================================================*/}
         {/*<button onClick={(event) => {myFirstSubscriber()}}>My YouTube Channel-1</button>*/}
         {/*<button onClick={(event) => {mySecondSubscriber()}}>My YouTube Channel-2</button>*/}
-        {/*=========================================================================================================*/}
+        {/*==========================================================================================================*/}
         {/*<button onClick={(event) => {console.log("Hello")}}>My YouTube Channel-2</button>*/}
-        {/*=========================================================================================================*/}
+        {/*==========================================================================================================*/}
         {/*<button onClick={(event) => {onClickHandler("Vasiliy")}}>My YouTube Channel-1</button>*/}
         {/*<button onClick={(event) => {onClickHandler("Ivan")}}>My YouTube Channel-1</button>*/}
-        {/*=========================================================================================================*/}
+        {/*==========================================================================================================*/}
         {/*<button onClick={massage_1}>1</button>*/}
         {/*<button onClick={() => {massage_2("+100500")}}>2</button>*/}
-        {/*=========================================================================================================*/}
-        {/*                <button>My YouTube Channel-1</button>*/}
-        {/*                <button>My YouTube Channel-2</button>*/}
-        {/*            <Header title={'New Header'}/>*/}
-        {/*            <Body title={'New Body'}/>*/}
-        {/*            <Footer title={'new Footer'}/>*/}
-        {/*            <NewComponent students={students}/>*/}
+        {/*==========================================================================================================*/}
+        {/*<button>My YouTube Channel-1</button>*/}
+        {/*<button>My YouTube Channel-2</button>*/}
+        {/*==========================================================================================================*/}
+        {/*==========================================================================================================*/}
         {/*<Button name="My YouTube Channel_1" callBack={() =>Button_1_Foo("I'm Vasia", 21, "A'm from Kharkiv")}/>*/}
         {/*<Button name="My YouTube Channel_2" callBack={() => Button_2_Foo("I'm Ivan")}/>*/}
         {/*<Button name="Button_3" callBack={Button_3_Foo}/>*/}
-        {/*=========================================================================================================*/}
+{/*//====================================================Lesson_4====================================================*/}
+        {/*<h1>{a}</h1>*/}
+        {/*<button onClick={onClickHandlerPlus}>plus</button>*/}
+        {/*<button onClick={onClickHandlerZero}>zero</button>*/}
+{/*//====================================================Lesson_5====================================================*/}
+            <ul>
+                {currentMoney.map((objFromMoneyArr, index) => {
+                    return (
+                        <li key={index}>
+                            <span>{objFromMoneyArr.banknote}</span>
+                            <span>{objFromMoneyArr.nominal}</span>
+                            <span>{objFromMoneyArr.number}</span>
+                        </li>
+                    )
+                })}
+            </ul>
+            <div style={{marginLeft:"45px"}}>
+                <button onClick={() => onClickFilterHandler("all")}>all</button>
+                <button onClick={() => onClickFilterHandler("dollar")}>dollars</button>
+                <button onClick={() => onClickFilterHandler("ruble")}>rubles</button>
+            </div>
 
-          <h1>{a}</h1>
-          <button onClick={onClickHandlerPlus}>plus</button>
-          <button onClick={onClickHandlerZero}>zero</button>
         </div>
       // </>
   )
